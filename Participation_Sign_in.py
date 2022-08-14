6cb802a1017122b5d600279da6ffacb20b5c5c40
Jforee <63127588+Jforee@users.noreply.github.com>
2022-08-14 09:30:01 -0700

    Update Participation_Sign_in.py

--- Participation_Sign_in.py
+++ Participation_Sign_in.py
@@ -1,45 +1,52 @@
 import xlsxwriter
+import PySimpleGUI as sg
 
-# Creating the workbook and worksheet
+sg.theme('DarkTeal9')
 
 wk_book = xlsxwriter.Workbook("Guest_Sign_In.xlsx")
 wk_sheet = wk_book.add_worksheet('Page1')
 
 # Create the rows for entry
 
+
 wk_sheet.write(0, 0, "Name")
-wk_sheet.write(0, 1, "appt")
+wk_sheet.write(0, 1, "title")
 wk_sheet.write(0, 2, "reason")
 wk_sheet.write(0, 3, "student_id")
 
-# Function to ask user input for their info and writes it in.
+# Layout for the window GUI
 
-def sign_in():
+layout = [
+    [sg.Text('Please fill out the following fields:')],
+    [sg.Text('Name', size=(15, 1)), sg.InputText(key="Name")],
+    [sg.Text('Title', size=(15, 1)), sg.InputText(key="title")],
+    [sg.Text('Reason', size=(15, 1)), sg.InputText(key='reason')],
+    [sg.Text('StudentID', size=(15, 1)), sg.InputText(key='student_id')],
 
-    # Gathering user input.
-    name = str(input("Name?:"))
-    appt = str(input("Appt? Y/N:"))
-    reason = str(input("Reason:"))
-    student_id = str(int(input("Student_ID:")))
+    [sg.Submit(), sg.Exit()]
+]
 
-    # Create the dictionary
-    data_list = [{
-        "Name": name,
-        "appt": appt,
-        "reason": reason,
-        "student_id": student_id,
-    }]
+window = sg.Window('Simple data entry form', layout)
 
-    # Writes information from dictionary into Excel sheet.
-    for index, entry in enumerate(data_list):
-        wk_sheet.write(index + 1, 0, entry["Name"])
-        wk_sheet.write(index + 1, 1, entry["appt"])
-        wk_sheet.write(index + 1, 2, entry["reason"])
-        wk_sheet.write(index + 1, 3, entry["student_id"])
+# Creating a list to store dictionary info.
 
+info_list = []
 
-sign_in()
+# functionality of the buttons
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Exit':  # Exit closes the window and Excel Workbook
+        break
+    if event == 'Submit':  # Submit takes the data from info_list and writes to Excel sheet.
+        s = info_list.append(values)
+        for index, entry in enumerate(info_list):
+            wk_sheet.write(index+1, 0, entry["Name"])
+            wk_sheet.write(index+1, 1, entry["title"])
+            wk_sheet.write(index+1, 2, entry["reason"])
+            wk_sheet.write(index+1, 3, entry["student_id"])
+
+        print(info_list)
 
 wk_book.close()
-
-
+window.close()
